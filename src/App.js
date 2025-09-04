19f26dc6dc52a5e7112039b68f457eda5666877d
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-04 12:59:48 +0700

    Looping

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import Body from "./components/Body";
 import Header from "./components/Header";
 import MyComponent from "./components/MyComponent";
 
+const products = [
+  {
+    id: 1,
+    name: "Wafer",
+    price: 20000,
+    discount: 20,
+  },
+  {
+    id: 2,
+    name: "Redmi Note X",
+    price: 12000000,
+    discount: 40,
+  },
+   {
+    id: 3,
+    name: "Bag",
+    price: 30000,
+    discount: 10,
+  },
+
+];
+
 function Product({ price, name, discount = 0 }) {
   return (
     <div>
@@ -25,8 +47,14 @@ function Product({ price, name, discount = 0 }) {
 function App() {
   return (
     <div className="App">
-      <Product name={"Wafer"} price={20000} discount={20} />
-      <Product name={"Redmi Note X"} price={12000000} discount={40} />
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          name={product.name}
+          price={product.price}
+          discount={product.discount}
+        />
+      ))}
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
