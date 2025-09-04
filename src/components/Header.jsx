b0aa37a9eed59669f737ba4b6ba3a902476ea128
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-04 13:52:47 +0700

    style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
   return (
     <>
-      <h1>Tutor ReactJS</h1>
+      <h1 style={{color: 'aquamarine', backgroundColor:"blue", padding: '10px'}}>Tutor ReactJS</h1>
       <h2>Panduan step-by-step ReactJS</h2>
     </>
   );
